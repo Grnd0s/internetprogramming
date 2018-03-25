662508a76cd097321590b685d4fed00fc9615d73
Pierre Verhaeghe <pverhaeghe@csumb.edu>
2018-03-25 09:32:23 +0000

    labs 5 fix

--- web/labs/lab5/functions.php
+++ web/labs/lab5/functions.php
@@ -16,7 +16,21 @@ function displayCart()
             echo "<td><img src='" . $item['img'] . "'></td>";
             echo "<td><h4>" . $item['name'] . "</h4></td>";
             echo "<td><h4>$" . $item['price'] . "</h4>></td>";
+            
+            echo '<form method="pos">';
+            echo "<input type='hidden' name='itemId' value='$itemId'>";
+            echo "<td><input type='text' name='update' class='form-control' placeHolder='$itemQuant'></td>";
+            echo '<td><button class="btn btn-danger">Update</button></td>';
+            echo '</form>';
+            
+            echo "<form method='post'>";
+            echo "<input type='hidden' name='removeId' value='$itemId'>";
+            echo '<td><button class="btn btn-danger">Remove</button></td>';
+            echo '</form>';
+            
+            echo '</tr>';
         }
+        echo "</table>";
     }
 }
 
@@ -28,6 +42,43 @@ function displayCartCount()
 }
 function displayResults()
 {
+    global $items;
     
+    if (isset($items))
+    {
+        echo "<table class='table'>";
+        foreach ($items as $item) 
+        {
+            $itemName = $item['name'];
+            $itemPrice = $item['salePrice'];
+            $itemImage = $item['thumbnailImage'];
+            $itemId = $item['id'];
+            
+            echo '<tr>';
+            
+            echo "<td><img src='" . $itemImage . "'></td>";
+            echo "<td><h4>" . $itemName . "</h4></td>";
+            echo "<td><h4>$" . $itemPrice . "</h4>></td>";
+            
+            echo '<form method="pos">';
+            echo "<input type='hidden' name='itemName' value='$itemName'>";
+            echo "<input type='hidden' name='itemPrice' value='$itemPrice'>";
+            echo "<input type='hidden' name='itemImg' value='$itemImage'>";
+            echo "<input type='hidden' name='itemId' value='$itemId'>";
+            
+            if ($_POST['itemId'] == $itemId)
+            {
+                echo '<td><button class="btn btn-success">Added</button></td>';
+            }
+            else 
+            {
+                echo '<td><button class="btn btn-warning">Add</button></td>';
+            }
+            
+            echo '</tr>';
+            echo '</form>';
+        }
+        echo "</table>";
+    }
 }
 ?>
