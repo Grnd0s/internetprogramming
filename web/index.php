1f0f465d33ae899ace4749446d9ac76fc2c58088
Pierre Verhaeghe <pverhaeghe@csumb.edu>
2018-04-20 06:59:09 +0000

    update index

--- web/index.php
+++ web/index.php
@@ -46,6 +46,7 @@ $app->run();
       <li><a href="labs/lab5/index.php"><h2>Lab 5</h2></a></li>
       <li><a href="labs/lab6/index.php"><h2>Lab 6</h2></a></li>
       <li><a href="labs/lab7/index.php"><h2>Lab 7</h2></a></li>
+      <li><a href="labs/lab8/index.php"><h2>Lab 8</h2></a></li>
     </ul>
     
     <h1>Homework</h1>
@@ -53,8 +54,10 @@ $app->run();
       <li><a href="hw1/index.html"><h1>Homework 1</h2></a></li>
       <li><a href="hw/hw2/index.php"><h2>Homework 2</h2></a></li>
       <li><a href="hw/hw3/index.php"><h2>Homework 3</h2></a></li>
+      <li><a href="hw/hw4/index.php"><h2>Homework 4</h2></a></li>
     </ul>
     
-    <a href="teamproject/index.php"><button class="btn btn-success">TeamProject</button></a>
+    <a href="teamproject/index.php"><button class="btn btn-success btn-lg">TeamProject</button></a><br />
+    <a href="finalproject/index.php"><button class="btn btn-primary btn-lg">Final Project</button></a>
   </body>
 </html>
