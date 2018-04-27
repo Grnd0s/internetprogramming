32c8d1798ff36d195f40dfa00d00db5e88a9f2cb
Pierre Verhaeghe <pverhaeghe@csumb.edu>
2018-04-27 02:28:06 +0000

    update index

--- web/index.php
+++ web/index.php
@@ -47,6 +47,7 @@ $app->run();
       <li><a href="labs/lab6/index.php"><h2>Lab 6</h2></a></li>
       <li><a href="labs/lab7/index.php"><h2>Lab 7</h2></a></li>
       <li><a href="labs/lab8/index.html"><h2>Lab 8</h2></a></li>
+      <li><a href="labs/lab9/signup.html"><h2>Lab 9</h2></a></li>
     </ul>
     
     <h1>Homework</h1>
